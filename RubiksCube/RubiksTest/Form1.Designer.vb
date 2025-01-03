8aa1fe46f7c5ede3a64816281d09660d7488623c
bordenaja <126289869+bordenaja@users.noreply.github.com>
2025-01-03 19:00:56 +0000

    Add files via upload

---
+++ RubiksCube/RubiksTest/Form1.Designer.vb
@@ -0,0 +1,495 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.btnScramble = New System.Windows.Forms.Button()
+        Me.udScramble = New System.Windows.Forms.NumericUpDown()
+        Me.btnHelp = New System.Windows.Forms.Button()
+        Me.btnL = New System.Windows.Forms.Button()
+        Me.btnR = New System.Windows.Forms.Button()
+        Me.btnU = New System.Windows.Forms.Button()
+        Me.btnD = New System.Windows.Forms.Button()
+        Me.btnF = New System.Windows.Forms.Button()
+        Me.btnB = New System.Windows.Forms.Button()
+        Me.btnBa = New System.Windows.Forms.Button()
+        Me.btnFa = New System.Windows.Forms.Button()
+        Me.btnDa = New System.Windows.Forms.Button()
+        Me.btnUa = New System.Windows.Forms.Button()
+        Me.btnRa = New System.Windows.Forms.Button()
+        Me.btnLa = New System.Windows.Forms.Button()
+        Me.btnza = New System.Windows.Forms.Button()
+        Me.btnya = New System.Windows.Forms.Button()
+        Me.btnxa = New System.Windows.Forms.Button()
+        Me.btnSa = New System.Windows.Forms.Button()
+        Me.btnEa = New System.Windows.Forms.Button()
+        Me.btnMa = New System.Windows.Forms.Button()
+        Me.btnz = New System.Windows.Forms.Button()
+        Me.btny = New System.Windows.Forms.Button()
+        Me.btnx = New System.Windows.Forms.Button()
+        Me.btnS = New System.Windows.Forms.Button()
+        Me.btnE = New System.Windows.Forms.Button()
+        Me.btnM = New System.Windows.Forms.Button()
+        Me.btnLog = New System.Windows.Forms.Button()
+        Me.PaletteY = New System.Windows.Forms.Button()
+        Me.PaletteW = New System.Windows.Forms.Button()
+        Me.PaletteB = New System.Windows.Forms.Button()
+        Me.PaletteG = New System.Windows.Forms.Button()
+        Me.PaletteR = New System.Windows.Forms.Button()
+        Me.PaletteO = New System.Windows.Forms.Button()
+        Me.txtPalette = New System.Windows.Forms.TextBox()
+        Me.chkEditingMode = New System.Windows.Forms.CheckBox()
+        Me.btnSolve = New System.Windows.Forms.Button()
+        CType(Me.udScramble, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(676, 76)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(75, 23)
+        Me.btnReset.TabIndex = 2
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'btnScramble
+        '
+        Me.btnScramble.Location = New System.Drawing.Point(676, 105)
+        Me.btnScramble.Name = "btnScramble"
+        Me.btnScramble.Size = New System.Drawing.Size(80, 35)
+        Me.btnScramble.TabIndex = 11
+        Me.btnScramble.Text = "Scramble"
+        Me.btnScramble.UseVisualStyleBackColor = True
+        '
+        'udScramble
+        '
+        Me.udScramble.Location = New System.Drawing.Point(630, 114)
+        Me.udScramble.Name = "udScramble"
+        Me.udScramble.Size = New System.Drawing.Size(40, 20)
+        Me.udScramble.TabIndex = 12
+        '
+        'btnHelp
+        '
+        Me.btnHelp.Location = New System.Drawing.Point(697, 12)
+        Me.btnHelp.Name = "btnHelp"
+        Me.btnHelp.Size = New System.Drawing.Size(75, 23)
+        Me.btnHelp.TabIndex = 13
+        Me.btnHelp.Text = "Help"
+        Me.btnHelp.UseVisualStyleBackColor = True
+        '
+        'btnL
+        '
+        Me.btnL.Location = New System.Drawing.Point(124, 315)
+        Me.btnL.Name = "btnL"
+        Me.btnL.Size = New System.Drawing.Size(75, 23)
+        Me.btnL.TabIndex = 14
+        Me.btnL.Text = "Button1"
+        Me.btnL.UseVisualStyleBackColor = True
+        '
+        'btnR
+        '
+        Me.btnR.Location = New System.Drawing.Point(204, 315)
+        Me.btnR.Name = "btnR"
+        Me.btnR.Size = New System.Drawing.Size(75, 23)
+        Me.btnR.TabIndex = 15
+        Me.btnR.Text = "Button1"
+        Me.btnR.UseVisualStyleBackColor = True
+        '
+        'btnU
+        '
+        Me.btnU.Location = New System.Drawing.Point(285, 315)
+        Me.btnU.Name = "btnU"
+        Me.btnU.Size = New System.Drawing.Size(75, 23)
+        Me.btnU.TabIndex = 16
+        Me.btnU.Text = "Button1"
+        Me.btnU.UseVisualStyleBackColor = True
+        '
+        'btnD
+        '
+        Me.btnD.Location = New System.Drawing.Point(366, 315)
+        Me.btnD.Name = "btnD"
+        Me.btnD.Size = New System.Drawing.Size(75, 23)
+        Me.btnD.TabIndex = 17
+        Me.btnD.Text = "Button1"
+        Me.btnD.UseVisualStyleBackColor = True
+        '
+        'btnF
+        '
+        Me.btnF.Location = New System.Drawing.Point(447, 315)
+        Me.btnF.Name = "btnF"
+        Me.btnF.Size = New System.Drawing.Size(75, 23)
+        Me.btnF.TabIndex = 18
+        Me.btnF.Text = "Button1"
+        Me.btnF.UseVisualStyleBackColor = True
+        '
+        'btnB
+        '
+        Me.btnB.Location = New System.Drawing.Point(528, 315)
+        Me.btnB.Name = "btnB"
+        Me.btnB.Size = New System.Drawing.Size(75, 23)
+        Me.btnB.TabIndex = 19
+        Me.btnB.Text = "Button1"
+        Me.btnB.UseVisualStyleBackColor = True
+        '
+        'btnBa
+        '
+        Me.btnBa.Location = New System.Drawing.Point(528, 344)
+        Me.btnBa.Name = "btnBa"
+        Me.btnBa.Size = New System.Drawing.Size(75, 23)
+        Me.btnBa.TabIndex = 25
+        Me.btnBa.Text = "Button1"
+        Me.btnBa.UseVisualStyleBackColor = True
+        '
+        'btnFa
+        '
+        Me.btnFa.Location = New System.Drawing.Point(447, 344)
+        Me.btnFa.Name = "btnFa"
+        Me.btnFa.Size = New System.Drawing.Size(75, 23)
+        Me.btnFa.TabIndex = 24
+        Me.btnFa.Text = "Button1"
+        Me.btnFa.UseVisualStyleBackColor = True
+        '
+        'btnDa
+        '
+        Me.btnDa.Location = New System.Drawing.Point(366, 344)
+        Me.btnDa.Name = "btnDa"
+        Me.btnDa.Size = New System.Drawing.Size(75, 23)
+        Me.btnDa.TabIndex = 23
+        Me.btnDa.Text = "Button1"
+        Me.btnDa.UseVisualStyleBackColor = True
+        '
+        'btnUa
+        '
+        Me.btnUa.Location = New System.Drawing.Point(285, 344)
+        Me.btnUa.Name = "btnUa"
+        Me.btnUa.Size = New System.Drawing.Size(75, 23)
+        Me.btnUa.TabIndex = 22
+        Me.btnUa.Text = "Button1"
+        Me.btnUa.UseVisualStyleBackColor = True
+        '
+        'btnRa
+        '
+        Me.btnRa.Location = New System.Drawing.Point(204, 344)
+        Me.btnRa.Name = "btnRa"
+        Me.btnRa.Size = New System.Drawing.Size(75, 23)
+        Me.btnRa.TabIndex = 21
+        Me.btnRa.Text = "Button1"
+        Me.btnRa.UseVisualStyleBackColor = True
+        '
+        'btnLa
+        '
+        Me.btnLa.Location = New System.Drawing.Point(123, 344)
+        Me.btnLa.Name = "btnLa"
+        Me.btnLa.Size = New System.Drawing.Size(75, 23)
+        Me.btnLa.TabIndex = 20
+        Me.btnLa.Text = "Button1"
+        Me.btnLa.UseVisualStyleBackColor = True
+        '
+        'btnza
+        '
+        Me.btnza.Location = New System.Drawing.Point(528, 402)
+        Me.btnza.Name = "btnza"
+        Me.btnza.Size = New System.Drawing.Size(75, 23)
+        Me.btnza.TabIndex = 37
+        Me.btnza.Text = "Button1"
+        Me.btnza.UseVisualStyleBackColor = True
+        '
+        'btnya
+        '
+        Me.btnya.Location = New System.Drawing.Point(447, 402)
+        Me.btnya.Name = "btnya"
+        Me.btnya.Size = New System.Drawing.Size(75, 23)
+        Me.btnya.TabIndex = 36
+        Me.btnya.Text = "Button1"
+        Me.btnya.UseVisualStyleBackColor = True
+        '
+        'btnxa
+        '
+        Me.btnxa.Location = New System.Drawing.Point(366, 402)
+        Me.btnxa.Name = "btnxa"
+        Me.btnxa.Size = New System.Drawing.Size(75, 23)
+        Me.btnxa.TabIndex = 35
+        Me.btnxa.Text = "Button1"
+        Me.btnxa.UseVisualStyleBackColor = True
+        '
+        'btnSa
+        '
+        Me.btnSa.Location = New System.Drawing.Point(285, 402)
+        Me.btnSa.Name = "btnSa"
+        Me.btnSa.Size = New System.Drawing.Size(75, 23)
+        Me.btnSa.TabIndex = 34
+        Me.btnSa.Text = "Button1"
+        Me.btnSa.UseVisualStyleBackColor = True
+        '
+        'btnEa
+        '
+        Me.btnEa.Location = New System.Drawing.Point(204, 402)
+        Me.btnEa.Name = "btnEa"
+        Me.btnEa.Size = New System.Drawing.Size(75, 23)
+        Me.btnEa.TabIndex = 33
+        Me.btnEa.Text = "Button1"
+        Me.btnEa.UseVisualStyleBackColor = True
+        '
+        'btnMa
+        '
+        Me.btnMa.Location = New System.Drawing.Point(123, 402)
+        Me.btnMa.Name = "btnMa"
+        Me.btnMa.Size = New System.Drawing.Size(75, 23)
+        Me.btnMa.TabIndex = 32
+        Me.btnMa.Text = "Button1"
+        Me.btnMa.UseVisualStyleBackColor = True
+        '
+        'btnz
+        '
+        Me.btnz.Location = New System.Drawing.Point(528, 373)
+        Me.btnz.Name = "btnz"
+        Me.btnz.Size = New System.Drawing.Size(75, 23)
+        Me.btnz.TabIndex = 31
+        Me.btnz.Text = "Button1"
+        Me.btnz.UseVisualStyleBackColor = True
+        '
+        'btny
+        '
+        Me.btny.Location = New System.Drawing.Point(447, 373)
+        Me.btny.Name = "btny"
+        Me.btny.Size = New System.Drawing.Size(75, 23)
+        Me.btny.TabIndex = 30
+        Me.btny.Text = "Button1"
+        Me.btny.UseVisualStyleBackColor = True
+        '
+        'btnx
+        '
+        Me.btnx.Location = New System.Drawing.Point(366, 373)
+        Me.btnx.Name = "btnx"
+        Me.btnx.Size = New System.Drawing.Size(75, 23)
+        Me.btnx.TabIndex = 29
+        Me.btnx.Text = "Button1"
+        Me.btnx.UseVisualStyleBackColor = True
+        '
+        'btnS
+        '
+        Me.btnS.Location = New System.Drawing.Point(285, 373)
+        Me.btnS.Name = "btnS"
+        Me.btnS.Size = New System.Drawing.Size(75, 23)
+        Me.btnS.TabIndex = 28
+        Me.btnS.Text = "Button1"
+        Me.btnS.UseVisualStyleBackColor = True
+        '
+        'btnE
+        '
+        Me.btnE.Location = New System.Drawing.Point(204, 373)
+        Me.btnE.Name = "btnE"
+        Me.btnE.Size = New System.Drawing.Size(75, 23)
+        Me.btnE.TabIndex = 27
+        Me.btnE.Text = "Button1"
+        Me.btnE.UseVisualStyleBackColor = True
+        '
+        'btnM
+        '
+        Me.btnM.Location = New System.Drawing.Point(123, 373)
+        Me.btnM.Name = "btnM"
+        Me.btnM.Size = New System.Drawing.Size(75, 23)
+        Me.btnM.TabIndex = 26
+        Me.btnM.Text = "Button1"
+        Me.btnM.UseVisualStyleBackColor = True
+        '
+        'btnLog
+        '
+        Me.btnLog.Location = New System.Drawing.Point(170, 30)
+        Me.btnLog.Name = "btnLog"
+        Me.btnLog.Size = New System.Drawing.Size(75, 23)
+        Me.btnLog.TabIndex = 38
+        Me.btnLog.Text = "Button1"
+        Me.btnLog.UseVisualStyleBackColor = True
+        '
+        'PaletteY
+        '
+        Me.PaletteY.Location = New System.Drawing.Point(15, 80)
+        Me.PaletteY.Name = "PaletteY"
+        Me.PaletteY.Size = New System.Drawing.Size(50, 50)
+        Me.PaletteY.TabIndex = 39
+        Me.PaletteY.Text = "Button1"
+        Me.PaletteY.UseVisualStyleBackColor = True
+        '
+        'PaletteW
+        '
+        Me.PaletteW.Location = New System.Drawing.Point(75, 80)
+        Me.PaletteW.Name = "PaletteW"
+        Me.PaletteW.Size = New System.Drawing.Size(50, 50)
+        Me.PaletteW.TabIndex = 40
+        Me.PaletteW.Text = "Button1"
+        Me.PaletteW.UseVisualStyleBackColor = True
+        '
+        'PaletteB
+        '
+        Me.PaletteB.Location = New System.Drawing.Point(15, 140)
+        Me.PaletteB.Name = "PaletteB"
+        Me.PaletteB.Size = New System.Drawing.Size(50, 50)
+        Me.PaletteB.TabIndex = 41
+        Me.PaletteB.Text = "Button1"
+        Me.PaletteB.UseVisualStyleBackColor = True
+        '
+        'PaletteG
+        '
+        Me.PaletteG.Location = New System.Drawing.Point(75, 140)
+        Me.PaletteG.Name = "PaletteG"
+        Me.PaletteG.Size = New System.Drawing.Size(50, 50)
+        Me.PaletteG.TabIndex = 42
+        Me.PaletteG.Text = "Button1"
+        Me.PaletteG.UseVisualStyleBackColor = True
+        '
+        'PaletteR
+        '
+        Me.PaletteR.Location = New System.Drawing.Point(15, 200)
+        Me.PaletteR.Name = "PaletteR"
+        Me.PaletteR.Size = New System.Drawing.Size(50, 50)
+        Me.PaletteR.TabIndex = 43
+        Me.PaletteR.Text = "Button1"
+        Me.PaletteR.UseVisualStyleBackColor = True
+        '
+        'PaletteO
+        '
+        Me.PaletteO.Location = New System.Drawing.Point(75, 200)
+        Me.PaletteO.Name = "PaletteO"
+        Me.PaletteO.Size = New System.Drawing.Size(50, 50)
+        Me.PaletteO.TabIndex = 44
+        Me.PaletteO.Text = "Button1"
+        Me.PaletteO.UseVisualStyleBackColor = True
+        '
+        'txtPalette
+        '
+        Me.txtPalette.Location = New System.Drawing.Point(15, 30)
+        Me.txtPalette.Name = "txtPalette"
+        Me.txtPalette.Size = New System.Drawing.Size(100, 20)
+        Me.txtPalette.TabIndex = 45
+        Me.txtPalette.Text = "Palette"
+        '
+        'chkEditingMode
+        '
+        Me.chkEditingMode.AutoSize = True
+        Me.chkEditingMode.Location = New System.Drawing.Point(15, 57)
+        Me.chkEditingMode.Name = "chkEditingMode"
+        Me.chkEditingMode.Size = New System.Drawing.Size(88, 17)
+        Me.chkEditingMode.TabIndex = 46
+        Me.chkEditingMode.Text = "Editing Mode"
+        Me.chkEditingMode.UseVisualStyleBackColor = True
+        '
+        'btnSolve
+        '
+        Me.btnSolve.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSolve.Location = New System.Drawing.Point(650, 210)
+        Me.btnSolve.Name = "btnSolve"
+        Me.btnSolve.Size = New System.Drawing.Size(120, 60)
+        Me.btnSolve.TabIndex = 47
+        Me.btnSolve.Text = "SOLVE!"
+        Me.btnSolve.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 461)
+        Me.Controls.Add(Me.btnSolve)
+        Me.Controls.Add(Me.chkEditingMode)
+        Me.Controls.Add(Me.txtPalette)
+        Me.Controls.Add(Me.PaletteO)
+        Me.Controls.Add(Me.PaletteR)
+        Me.Controls.Add(Me.PaletteG)
+        Me.Controls.Add(Me.PaletteB)
+        Me.Controls.Add(Me.PaletteW)
+        Me.Controls.Add(Me.PaletteY)
+        Me.Controls.Add(Me.btnLog)
+        Me.Controls.Add(Me.btnza)
+        Me.Controls.Add(Me.btnya)
+        Me.Controls.Add(Me.btnxa)
+        Me.Controls.Add(Me.btnSa)
+        Me.Controls.Add(Me.btnEa)
+        Me.Controls.Add(Me.btnMa)
+        Me.Controls.Add(Me.btnz)
+        Me.Controls.Add(Me.btny)
+        Me.Controls.Add(Me.btnx)
+        Me.Controls.Add(Me.btnS)
+        Me.Controls.Add(Me.btnE)
+        Me.Controls.Add(Me.btnM)
+        Me.Controls.Add(Me.btnBa)
+        Me.Controls.Add(Me.btnFa)
+        Me.Controls.Add(Me.btnDa)
+        Me.Controls.Add(Me.btnUa)
+        Me.Controls.Add(Me.btnRa)
+        Me.Controls.Add(Me.btnLa)
+        Me.Controls.Add(Me.btnB)
+        Me.Controls.Add(Me.btnF)
+        Me.Controls.Add(Me.btnD)
+        Me.Controls.Add(Me.btnU)
+        Me.Controls.Add(Me.btnR)
+        Me.Controls.Add(Me.btnL)
+        Me.Controls.Add(Me.btnHelp)
+        Me.Controls.Add(Me.udScramble)
+        Me.Controls.Add(Me.btnScramble)
+        Me.Controls.Add(Me.btnReset)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.udScramble, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnReset As Button
+    Friend WithEvents btnScramble As Button
+    Friend WithEvents udScramble As NumericUpDown
+    Friend WithEvents btnHelp As Button
+    Friend WithEvents btnL As Button
+    Friend WithEvents btnR As Button
+    Friend WithEvents btnU As Button
+    Friend WithEvents btnD As Button
+    Friend WithEvents btnF As Button
+    Friend WithEvents btnB As Button
+    Friend WithEvents btnBa As Button
+    Friend WithEvents btnFa As Button
+    Friend WithEvents btnDa As Button
+    Friend WithEvents btnUa As Button
+    Friend WithEvents btnRa As Button
+    Friend WithEvents btnLa As Button
+    Friend WithEvents btnza As Button
+    Friend WithEvents btnya As Button
+    Friend WithEvents btnxa As Button
+    Friend WithEvents btnSa As Button
+    Friend WithEvents btnEa As Button
+    Friend WithEvents btnMa As Button
+    Friend WithEvents btnz As Button
+    Friend WithEvents btny As Button
+    Friend WithEvents btnx As Button
+    Friend WithEvents btnS As Button
+    Friend WithEvents btnE As Button
+    Friend WithEvents btnM As Button
+    Friend WithEvents btnLog As Button
+    Friend WithEvents PaletteY As Button
+    Friend WithEvents PaletteW As Button
+    Friend WithEvents PaletteB As Button
+    Friend WithEvents PaletteG As Button
+    Friend WithEvents PaletteR As Button
+    Friend WithEvents PaletteO As Button
+    Friend WithEvents txtPalette As TextBox
+    Friend WithEvents btnSolve As Button
+    Public WithEvents chkEditingMode As CheckBox
+End Class
---
+++ RubiksCube/RubiksTest/PaletteChanges.vb
@@ -0,0 +1,209 @@
+﻿Module PaletteChanges
+    Public currentpalettecolour As Color
+    Public Sub PaletteY_click(ByRef faceletface As String, ByRef paletteselected As Boolean,
+                       ByRef FaceY As Face, ByRef FaceW As Face,
+                       ByRef FaceB As Face, ByRef FaceG As Face,
+                       ByRef FaceR As Face, ByRef FaceO As Face)
+        Dim i As Integer
+        Dim j As Integer
+        Dim faceletcolour As Char
+        currentpalettecolour = Color.Yellow
+        If faceletface = "" Then
+            paletteselected = True
+        Else
+            faceletcolour = Mid(faceletface, 1, 1)
+            i = Int(Mid(faceletface, 2, 1))
+            j = Int(Mid(faceletface, 3, 1))
+            Select Case faceletcolour
+                Case "Y"
+                    FaceY.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "W"
+                    FaceW.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "B"
+                    FaceB.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "G"
+                    FaceG.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "R"
+                    FaceR.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "O"
+                    FaceO.faceletsArray(i, j).BackColor = currentpalettecolour
+            End Select
+        End If
+    End Sub
+
+    Public Sub PaletteW_click(ByVal faceletface As String, ByRef paletteselected As Boolean,
+                       ByRef FaceY As Face, ByRef FaceW As Face,
+                       ByRef FaceB As Face, ByRef FaceG As Face,
+                       ByRef FaceR As Face, ByRef FaceO As Face)
+        Dim i As Integer
+        Dim j As Integer
+        Dim faceletcolour As Char
+        currentpalettecolour = Color.White
+        If faceletface = "" Then
+            paletteselected = True
+        Else
+            faceletcolour = Mid(faceletface, 1, 1)
+            i = Int(Mid(faceletface, 2, 1))
+            j = Int(Mid(faceletface, 3, 1))
+            Select Case faceletface
+                Case "Y"
+                    FaceY.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "W"
+                    FaceW.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "B"
+                    FaceB.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "G"
+                    FaceG.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "R"
+                    FaceR.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "O"
+                    FaceO.faceletsArray(i, j).BackColor = currentpalettecolour
+            End Select
+        End If
+    End Sub
+
+    Public Sub PaletteB_click(ByVal faceletface As String, ByRef paletteselected As Boolean,
+                       ByRef FaceY As Face, ByRef FaceW As Face,
+                       ByRef FaceB As Face, ByRef FaceG As Face,
+                       ByRef FaceR As Face, ByRef FaceO As Face)
+        Dim i As Integer
+        Dim j As Integer
+        Dim faceletcolour As Char
+        currentpalettecolour = Color.Blue
+        If faceletface = "" Then
+            paletteselected = True
+        Else
+            faceletcolour = Mid(faceletface, 1, 1)
+            i = Int(Mid(faceletface, 2, 1))
+            j = Int(Mid(faceletface, 3, 1))
+            Select Case faceletface
+                Case "Y"
+                    FaceY.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "W"
+                    FaceW.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "B"
+                    FaceB.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "G"
+                    FaceG.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "R"
+                    FaceR.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "O"
+                    FaceO.faceletsArray(i, j).BackColor = currentpalettecolour
+            End Select
+        End If
+    End Sub
+
+    Public Sub PaletteG_click(ByVal faceletface As String, ByRef paletteselected As Boolean,
+                       ByRef FaceY As Face, ByRef FaceW As Face,
+                       ByRef FaceB As Face, ByRef FaceG As Face,
+                       ByRef FaceR As Face, ByRef FaceO As Face)
+        Dim i As Integer
+        Dim j As Integer
+        Dim faceletcolour As Char
+        currentpalettecolour = Color.Green
+        If faceletface = "" Then
+            paletteselected = True
+        Else
+            faceletcolour = Mid(faceletface, 1, 1)
+            i = Int(Mid(faceletface, 2, 1))
+            j = Int(Mid(faceletface, 3, 1))
+            Select Case faceletface
+                Case "Y"
+                    FaceY.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "W"
+                    FaceW.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "B"
+                    FaceB.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "G"
+                    FaceG.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "R"
+                    FaceR.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "O"
+                    FaceO.faceletsArray(i, j).BackColor = currentpalettecolour
+            End Select
+        End If
+    End Sub
+
+    Public Sub PaletteR_click(ByVal faceletface As String, ByRef paletteselected As Boolean,
+                       ByRef FaceY As Face, ByRef FaceW As Face,
+                       ByRef FaceB As Face, ByRef FaceG As Face,
+                       ByRef FaceR As Face, ByRef FaceO As Face)
+        Dim i As Integer
+        Dim j As Integer
+        Dim faceletcolour As Char
+        currentpalettecolour = Color.Red
+        If faceletface = "" Then
+            paletteselected = True
+        Else
+            faceletcolour = Mid(faceletface, 1, 1)
+            i = Int(Mid(faceletface, 2, 1))
+            j = Int(Mid(faceletface, 3, 1))
+            Select Case faceletface
+                Case "Y"
+                    FaceY.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "W"
+                    FaceW.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "B"
+                    FaceB.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "G"
+                    FaceG.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "R"
+                    FaceR.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "O"
+                    FaceO.faceletsArray(i, j).BackColor = currentpalettecolour
+            End Select
+        End If
+    End Sub
+
+    Public Sub PaletteO_click(ByVal faceletface As String, ByRef paletteselected As Boolean,
+                       ByRef FaceY As Face, ByRef FaceW As Face,
+                       ByRef FaceB As Face, ByRef FaceG As Face,
+                       ByRef FaceR As Face, ByRef FaceO As Face)
+        Dim i As Integer
+        Dim j As Integer
+        Dim faceletcolour As Char
+        currentpalettecolour = Color.Orange
+        If faceletface = "" Then
+            paletteselected = True
+        Else
+            faceletcolour = Mid(faceletface, 1, 1)
+            i = Int(Mid(faceletface, 2, 1))
+            j = Int(Mid(faceletface, 3, 1))
+            Select Case faceletface
+                Case "Y"
+                    FaceY.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "W"
+                    FaceW.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "B"
+                    FaceB.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "G"
+                    FaceG.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "R"
+                    FaceR.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "O"
+                    FaceO.faceletsArray(i, j).BackColor = currentpalettecolour
+            End Select
+        End If
+    End Sub
+
+    Public Sub faceletclicked(faceinitial As Char, i As Integer, j As Integer)
+        If Form1.paletteselected = True Then
+            Select Case faceinitial
+                Case "W"
+                    Form1.FaceW.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "Y"
+                    Form1.FaceY.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "B"
+                    Form1.FaceB.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "G"
+                    Form1.FaceG.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "R"
+                    Form1.FaceR.faceletsArray(i, j).BackColor = currentpalettecolour
+                Case "O"
+                    Form1.FaceO.faceletsArray(i, j).BackColor = currentpalettecolour
+            End Select
+        Else
+            Form1.faceletface = faceinitial + Str(i) + Str(j)
+        End If
+    End Sub
+End Module
